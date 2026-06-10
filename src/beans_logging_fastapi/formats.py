0aaef1792a187da56c0a8f0ba826ee492a83f604
Batkhuu Byambajav <batkhuu10@gmail.com>
2026-06-10 13:25:26 +0900

    fix: reorder user_id and trace_id in log formats for consistency.

--- src/beans_logging_fastapi/formats.py
+++ src/beans_logging_fastapi/formats.py
@@ -99,17 +99,17 @@ def id_std_format(record: "Record") -> str:
         str: Format for std output log with user_id, trace_id, and request_id.
     """
 
-    _user_id = record["extra"].get("user_id")
-    _trace_id = record["extra"].get("trace_id")
     _request_id = record["extra"].get("request_id")
-    _user_id_part = f" | <m>{_user_id}</m>" if _user_id else ""
-    _trace_id_part = f" | <lc>{_trace_id}</lc>" if _trace_id else ""
+    _trace_id = record["extra"].get("trace_id")
+    _user_id = record["extra"].get("user_id")
     _request_id_part = f" | <d><w>{_request_id}</w></d>" if _request_id else ""
+    _trace_id_part = f" | <lc>{_trace_id}</lc>" if _trace_id else ""
+    _user_id_part = f" | <m>{_user_id}</m>" if _user_id else ""
 
     _format = (
-        "[<c>{time:YYYY-MM-DD HH:mm:ss.SSS Z}</c> | <level>{extra[level_short]:<5}</level> | <w>{name}:{line}</w>"
-        f"{_user_id_part}{_trace_id_part}{_request_id_part}"
-        "]: <level>{message}</level>\n{exception}"
+        "[<c>{time:YYYY-MM-DD HH:mm:ss.SSS Z}</c> | <level>{extra[level_short]:<5}</level>"
+        f"{_request_id_part}{_trace_id_part}{_user_id_part}"
+        " | <w>{name}:{line}</w>]: <level>{message}</level>\n{exception}"
     )
     return _format
 
@@ -124,16 +124,16 @@ def id_file_format(record: "Record") -> str:
         str: Format for file log with user_id, trace_id, and request_id.
     """
 
-    _user_id = record["extra"].get("user_id")
-    _trace_id = record["extra"].get("trace_id")
     _request_id = record["extra"].get("request_id")
-    _user_id_part = f" | {_user_id}" if _user_id else ""
-    _trace_id_part = f" | {_trace_id}" if _trace_id else ""
+    _trace_id = record["extra"].get("trace_id")
+    _user_id = record["extra"].get("user_id")
     _request_id_part = f" | {_request_id}" if _request_id else ""
+    _trace_id_part = f" | {_trace_id}" if _trace_id else ""
+    _user_id_part = f" | {_user_id}" if _user_id else ""
 
     _format = (
         "[{time:YYYY-MM-DD HH:mm:ss.SSS Z} | {extra[level_short]:<5} | {name}:{line}"
-        f"{_user_id_part}{_trace_id_part}{_request_id_part}"
+        f"{_request_id_part}{_trace_id_part}{_user_id_part}"
         "]: {message}\n{exception}"
     )
     return _format
